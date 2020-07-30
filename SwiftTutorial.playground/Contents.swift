6dcbf44c858e2f3d982f1be9662769a0766b06e2
jpalmat <jimmy.palma1990@gmail.com>
2020-07-30 13:40:28 -0500

    variable types

--- SwiftTutorial.playground/Contents.swift
+++ SwiftTutorial.playground/Contents.swift
@@ -2,3 +2,10 @@ import Cocoa
 
 var str = "Hello, playground"
 print("max int \(Int.max)")
+var pi2: Float = 3.1415
+var pi1: Double = 3.1415
+print(pi2)
+print(pi1)
+//random
+let ram = Int.random(in: 1...10)
+print(ram)
